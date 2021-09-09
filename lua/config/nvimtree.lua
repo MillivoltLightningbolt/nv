1aa2aa61e270d9e46db97db436add7a4219a6e94
MillivoltLightningbolt <h.kaplan@gmx.net>
2021-09-09 19:50:30 +0200

    customized nvimtree settings

--- lua/config/nvimtree.lua
+++ lua/config/nvimtree.lua
@@ -1,9 +1,11 @@
-vim.g.nvim_tree_side = "right"
+vim.g.nvim_tree_side = "left"
 vim.g.nvim_tree_ignore = { ".git", "node_modules", "target" }
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_auto_open = 0
+vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_resize = 0
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_update_cwd = 1
 vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
 vim.g.nvim_tree_special_files = { ["README.md"] = 1 }
+vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
+--vim.g.nvim_tree_auto_resize = 0 -- 1 by default, will resize the tree to its saved width when opening a file
